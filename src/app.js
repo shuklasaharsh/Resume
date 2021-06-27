f02debc34a5f3b4496e30c69d318020129f58a70
Saharsh Shukla <saharsh.shukla2018@vitstudent.ac.in>
2021-06-28 00:06:23 +0530

    Add Backend Functionality

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const path = require('path')
 // NPM modules
 const express = require('express')
 const hbs = require('hbs')
+const request = require('postman-request')
+// Files
+const getGeocode = require('../utils/getGeocode')
+const getWeather = require('../utils/getWeather')
 const app = express()
 //Define Paths for Express Configuration
 
@@ -16,13 +20,7 @@ hbs.registerPartials(partialsPath)
 //Setup Static directory to serve
 app.use(express.static(htmlPath))
 //Routes
-app.get('/weather', (req,res)=>{
-    const body = {
-        forecast: 'Something',
-        location: 'Something2'
-    }
-    res.send(body)
-})
+
 // INDEX
 app.get('', (req,res)=>{
     res.render('index', {
@@ -46,13 +44,34 @@ app.get('/help', (req,res)=>{
         name: 'Saharsh Shukla'
     })
 })
-
+//HELP ERROR
 app.get('/help/*', (req,res)=>{
     res.render('404', {
         name: 'Saharsh Shukla',
         title: 'Error 404',
         errorMessage: 'Error Occurred - Help not found'
     })
+})
+//WEATHER
+app.get('/weather', (req,res)=> {
+    if (!req.query.location) {
+        return res.send({
+            error: 'Null Location Error: please send a valid location'
+        })
+    }
+    getGeocode(req.query.location, (error, body)=>{
+        getWeather(body,(error, {temperature, feelsLike, precipitation, currentTime})=>{
+            res.send({
+                temperature, feelsLike, precipitation, currentTime,
+                location: body
+            })
+        })
+    })
+
+/*    res.send({
+        location: req.query.location,
+        forecast: "This is the forecast"
+    })*/
 })
 
 
---
+++ utils/getGeocode.js
@@ -0,0 +1,33 @@
+const requests = require('postman-request')
+const CONSTANTS = require('../data/CONSTANTS')
+const geocodeConstants = CONSTANTS.geocodeConstants
+
+const getGeocode = (address, callback) => {
+    const url = geocodeConstants.baseurl + encodeURIComponent(address) + '.json?' + geocodeConstants.api + '&limit1'
+    console.log(url)
+    requests({url: url, json: true}, (error, {body}={}) => {
+        try {
+            const location = {
+                coordinates: body.features[0].center,
+                location: body.features[0].place_name,
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0]
+            }
+            callback(undefined, location)
+        } catch (e) {
+            let e2 = e.toString()
+            if (error) {
+                e2 = 'OS Error: ' + error
+                callback(e2, undefined)
+            } else if (body.message) {
+                callback(body.message, undefined)
+            } else if (body.features) {
+                e2 = "Invalid Query Error"
+                callback(e2, undefined)
+            }
+        }
+    })
+}
+
+
+module.exports = getGeocode
